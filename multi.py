eeec34e6eb3f29fc842601f043e31c924df6642e
amirnsahmad <32689056+amirnsahmad@users.noreply.github.com>
2021-04-12 16:41:36 -0300

    Create multi.py

---
+++ multi.py
@@ -0,0 +1,28 @@
+import argparse
+import os
+import time
+import sys
+
+Parser = argparse.ArgumentParser()
+Parser.add_argument('-l', '--list', help="URL Lists")
+Parser.add_argument('-w', '--worker', default=5.0, help="Workers. Default: 5")
+Parser.add_argument('-c', '--config', default="default.py", help="Filepath to the configuration file of payloads")
+Args = Parser.parse_args()
+
+
+workers=int(Args.worker)
+print(workers)
+if not Args.list or not os.path.isfile(Args.list):
+        if sys.stdin.isatty():
+                print("No URLs.")
+                exit(1)
+        else:
+                lines=sys.stdin.read().split("\n")
+else:
+        lines=open(Args.list,'r')
+for line in lines:
+        while int(os.popen("ps aux | grep smuggler | grep -c -v grep").read().replace("\n",""))  >= workers:
+                time.sleep(60)
+        os.system("/usr/bin/smuggler -x -c "+Args.config+" -u "+line.replace("\n","")+" &")
+while int(os.popen("ps aux | grep smuggler | grep -c -v grep").read().replace("\n","")) != 0:
+        time.sleep(60)
