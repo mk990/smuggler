36bf2aee7cc06ab6581160f4abba8672fded03ce
amirnsahmad <32689056+amirnsahmad@users.noreply.github.com>
2021-04-15 13:52:51 -0300

    Update multi.py

--- multi.py
+++ multi.py
@@ -1,28 +1,41 @@
+import concurrent.futures
+import logging
+import threading
+import time
 import argparse
 import os
 import time
 import sys
+global Args
 
-Parser = argparse.ArgumentParser()
-Parser.add_argument('-l', '--list', help="URL Lists")
-Parser.add_argument('-w', '--worker', default=5.0, help="Workers. Default: 5")
-Parser.add_argument('-c', '--config', default="default.py", help="Filepath to the configuration file of payloads")
-Args = Parser.parse_args()
+def thread_function(line):
+    os.system("/usr/bin/smuggler -x -c "+Args.config+" -u "+line.replace("\n",""))
 
+if __name__ == "__main__":
 
-workers=int(Args.worker)
-print(workers)
-if not Args.list or not os.path.isfile(Args.list):
+    Parser = argparse.ArgumentParser()
+    Parser.add_argument('-l', '--list', help="URL Lists")
+    Parser.add_argument('-w', '--worker', default=5.0, help="Workers. Default: 5")
+    Parser.add_argument('-c', '--config', default="default.py", help="Filepath to the configuration file of payloads")
+    Args = Parser.parse_args()
+#    global Args
+    workers=int(Args.worker)
+    print(workers)
+    if not Args.list or not os.path.isfile(Args.list):
         if sys.stdin.isatty():
                 print("No URLs.")
                 exit(1)
         else:
                 lines=sys.stdin.read().split("\n")
-else:
+    else:
         lines=open(Args.list,'r')
-for line in lines:
-        while int(os.popen("ps aux | grep smuggler | grep -c -v grep").read().replace("\n",""))  >= workers:
-                time.sleep(60)
-        os.system("/usr/bin/smuggler -x -c "+Args.config+" -u "+line.replace("\n","")+" &")
-while int(os.popen("ps aux | grep smuggler | grep -c -v grep").read().replace("\n","")) != 0:
-        time.sleep(60)
+#    for line in lines:
+#    format = "%(asctime)s: %(message)s"
+ #   logging.basicConfig(format=format, level=logging.INFO,
+#                        datefmt="%H:%M:%S")
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers) as executor:
+        executor.map(thread_function, lines)
+
+
+
