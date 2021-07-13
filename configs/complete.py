fa3420e74d4e61fbd21be7ca2ee64eb256b652f6
amirnsahmad <32689056+amirnsahmad@users.noreply.github.com>
2021-07-13 19:55:18 -0300

    Update complete.py

--- configs/complete.py
+++ configs/complete.py
@@ -2,7 +2,7 @@
 def render_template(gadget):
         RN = "\r\n"
         p = Payload()
-        p.header  = "__METHOD__ __ENDPOINT__?cb=__RANDOM__ HTTP/1.1" + RN
+        p.header  = "__METHOD__ __ENDPOINT__?cb=__RANDOM__ HTTP/__HTTP_VERSION__" + RN
         p.header += gadget + RN
         p.header += "Host: __HOST__" + RN
         p.header += "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36" + RN
