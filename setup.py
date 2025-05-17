e13fb5965439ff1c90f7f2d9704fe4a6a02ebc13
Mohammad Hemmati <m.kemeshki@gmail.com>
2025-05-17 06:26:48 +0330

    add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='smuggler',
+    version='1.1.0',
+    description='HTTP Request Smuggling Detection Tool',
+    long_description="""
+    Smuggler is a tool for detecting HTTP request smuggling vulnerabilities.
+    It works by sending crafted requests to test for CL.TE and TE.CL desync behaviors.
+    """,
+    url='https://github.com/mk990/smuggler ',
+    license='MIT',
+    py_modules=['smuggler'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+        'Topic :: Internet :: WWW/HTTP'
+    ],
+    packages=['configs','payloads'],  # Include configs as a package
+    entry_points={
+        'console_scripts': [
+            'smuggler = smuggler:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
